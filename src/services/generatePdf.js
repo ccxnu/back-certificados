f7d1a716c8db8851776e0179ab16f56ba5a9f882
Pablo Cuenca <108037606+ccxnu@users.noreply.github.com>
2024-05-17 23:55:45 -0500

    Update

--- src/services/generatePdf.js
+++ src/services/generatePdf.js
@@ -6,7 +6,13 @@ import "dotenv/config";
 
 export const generatePdf = async ({ nombre, apellido, saldo, qrcode }) => {
   try {
-    // Create a new browser instance
+    // Read the contents of the HTML template file
+    const data = await fs.readFile("./public/template.html", "utf-8");
+    // Compile the template with Handlebars
+    const template = handlebars.compile(data);
+    // Fill the template with certificate data
+    const html = template({ nombre, apellido, saldo, qrcode });
+
     const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox", "--no-zygote"],
       executablePath:
@@ -15,12 +21,6 @@ export const generatePdf = async ({ nombre, apellido, saldo, qrcode }) => {
           : puppeteer.executablePath(),
     });
     const page = await browser.newPage();
-    // Read the contents of the HTML template file
-    const data = await fs.readFile("./public/template.html", "utf-8");
-    // Compile the template with Handlebars
-    const template = handlebars.compile(data);
-    // Fill the template with certificate data
-    const html = template({ nombre, apellido, saldo, qrcode });
 
     await page.setContent(html);
     // Generate PDF
@@ -29,7 +29,6 @@ export const generatePdf = async ({ nombre, apellido, saldo, qrcode }) => {
     await browser.close();
     // Convert PDF buffer to base64
     const pdfBase64 = pdfBuffer.toString("base64");
-
     return pdfBase64;
   } catch (error) {
     const err = createThrowError("Algo ha ocurrido al generar el pdf.", 500);
